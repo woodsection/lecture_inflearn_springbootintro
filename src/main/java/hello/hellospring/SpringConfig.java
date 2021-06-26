01baf7fae9c3ac36b1787a0f08f50b46525f322d
kmd <wd.section@gmail.com>
2021-06-26 16:34:15 +0900

    jdbcTemplate code

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,12 +1,25 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateRepository;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -14,7 +27,9 @@ public class SpringConfig {
     }
 
     @Bean
-    public MemoryMemberRepository memoryMemberRepository() {
-        return new MemoryMemberRepository();
+    public MemberRepository memoryMemberRepository() {
+//        return new MemoryMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateRepository(dataSource);
     }
 }
